c66f5dfd8d5a3180cddded32367783fed674d917
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-11-09 15:24:25 +0900

    에러핸들러 적용

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app = express();
 require("dotenv").config();
 const port = process.env.port;
 
+const errorHandler = require("./middlewares/errorHandler");
+
 app.use(express.json());
 const Router = require("./routes/index");
 app.use("/", Router);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,14 +1,17 @@
+const ErrorCustom = require("../middlewares/errorCustom");
 const UserService = require("../services/user.service");
 
 class UserController {
   userService = new UserService();
 
-  createUser = async (req, res) => {
+  createUser = async (req, res, next) => {
     try {
       const userId = await this.userService.createUser();
 
       res.status(201).json({ userId: userId.userId });
-    } catch {}
+    } catch (err) {
+      next(err);
+    }
   };
 }
 
